5f0d7e6e475708e11533b5be147886d71a1bab43
Eirian-Ta <36525335+Eirian-Ta@users.noreply.github.com>
2018-11-26 13:18:20 -0500

    Update page-find-us.php

--- themes/inhabitent_theme/page-find-us.php
+++ themes/inhabitent_theme/page-find-us.php
@@ -19,8 +19,7 @@ get_header(); ?>
 							width="760"
 							height="300"
 							frameborder="0" style="border:0"
-							src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDWP_pLEwWYQpc00qEHRkjzMZ8WyhiUNRU
-								&q=1490+W+Broadway,+Vancouver,+BC+V6H" allowfullscreen>
+							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2603.6833050880177!2d-123.14035688431126!3d49.26344827932923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673c79e1ac457%3A0x3aea6428fa30dc6a!2s1490+W+Broadway%2C+Vancouver%2C+BC+V6H+4E8!5e0!3m2!1svi!2sca!4v1543255923833H" allowfullscreen>
 						</iframe>
 						<?php echo '<h2>'.CFS()->get('find_us_title').'</h2>';?>
 						<?php echo '<p>'.CFS()->get('find_us_content').'</p>';?>
